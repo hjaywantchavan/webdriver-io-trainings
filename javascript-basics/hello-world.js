ebf9f8111d19f8936de847ea5429921c71f31f22
hjaywantchavan <hjaywantchavan@deloitte.com>
2023-05-27 20:15:30 +0530

    javascript sessions

---
+++ javascript-basics/hello-world.js
@@ -0,0 +1,16 @@
+// Print messages
+console.log("Hello World!")
+
+// Get type of variable
+let a = 35.6
+console.log('Data Type of '+a+' is '+typeof(a))
+
+let b = 'Harshal Chavan'
+console.log('Data Type of '+b+' is '+typeof(b))
+
+let c = true
+console.log('Data Type of '+c+' is '+typeof(c))
+
+// Two other commonly used data types - null and undefined
+
+
---
+++ javascript-basics/loops-and-conditions.js
@@ -0,0 +1,25 @@
+let flag = true;
+
+// Conditional statements
+if(flag){
+    console.log("Condition is "+flag)
+}
+else{
+    console.log("Condition is "+flag)
+}
+
+// Loops
+
+// 1. While loop
+console.log("While loop")
+let s = 20
+while(s>10){
+    console.log(s)
+    s--;
+}
+
+// For loops
+console.log("For loop")
+for(let k=0; k<=10;k++){
+    console.log(k)
+}
