56ba5267e0845d12eec406283f71043b29f3554c
hjaywantchavan <hjaywantchavan@deloitte.com>
2023-05-28 12:30:04 +0530

    multiple conditions in for loop

--- javascript-basics/loops-and-conditions.js
+++ javascript-basics/loops-and-conditions.js
@@ -20,6 +20,11 @@ while(s>10){
 
 // For loops
 console.log("For loop")
-for(let k=0; k<=10;k++){
-    console.log(k)
+let n = 0;
+for(let k=1; k<=100,n<=2;k++){
+    if(k%2 ==0 && k%5==0){
+        n++;
+        console.log(k)
+    } 
 }
+
