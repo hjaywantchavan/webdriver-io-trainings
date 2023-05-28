e2572b719975df5dbbd008db18aef331d249cc4d
hjaywantchavan <hjaywantchavan@deloitte.com>
2023-05-28 15:26:08 +0530

    functions

---
+++ javascript-basics/functions.js
@@ -0,0 +1,22 @@
+function addition(a,b){
+    return a+b
+}
+
+function subtract(a,b){
+    return a-b
+};
+
+let division = function(a,b){
+    return a/b
+}
+
+let multiplication = (a,b)=> a*b
+
+let sum = addition(23,56)
+console.log(sum)
+
+sum = division(3,4)
+console.log(sum)
+
+sum = multiplication(3,4)
+console.log(sum)
